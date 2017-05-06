8b380ab52fb6233472b2762ff9dba6d0f4ced99f
Niv Shah <nivshah@gmail.com>
2017-05-06 18:10:27 -0400

    Basic admin links
    Need to make this a little cleaner separation of logic / view but fine
    for now

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,10 @@ class Link < ApplicationRecord
   belongs_to :user
   belongs_to :list
   belongs_to :category
+
+  def self.created_for_today_by_user(user)
+    if List.created_for_today
+      return Link.find_by(user: user, list: List.last)
+    end
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,9 @@
 class List < ApplicationRecord
   has_many :links
+
+  def self.created_for_today
+    list = List.last
+
+    return list && list.publish_date == Date.today
+  end
 end
