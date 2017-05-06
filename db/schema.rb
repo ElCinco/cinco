a77a485e60a6f9d5a4b68d839d42c5f16a58a706
Niv Shah <nivshah@gmail.com>
2017-05-06 18:47:34 -0400

    Switched over to PostGres like such a boss

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170506203452) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "label"
     t.string   "color"
@@ -27,9 +30,9 @@ ActiveRecord::Schema.define(version: 20170506203452) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_links_on_category_id"
-    t.index ["list_id"], name: "index_links_on_list_id"
-    t.index ["user_id"], name: "index_links_on_user_id"
+    t.index ["category_id"], name: "index_links_on_category_id", using: :btree
+    t.index ["list_id"], name: "index_links_on_list_id", using: :btree
+    t.index ["user_id"], name: "index_links_on_user_id", using: :btree
   end
 
   create_table "lists", force: :cascade do |t|
@@ -52,9 +55,12 @@ ActiveRecord::Schema.define(version: 20170506203452) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "links", "categories"
+  add_foreign_key "links", "lists"
+  add_foreign_key "links", "users"
 end
