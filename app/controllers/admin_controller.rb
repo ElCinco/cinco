b06186155f46865f0141b1062b1c8e38c57d86cc
Niv Shah <nivshah@gmail.com>
2017-05-06 15:16:22 -0400

    Admin controller

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,14 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def create_user
+  end
+
+  def create_list
+  end
+
+  def create_link
+  end
+end
