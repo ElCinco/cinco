5b36d4e0c5fdc2be3350d2c7a2c62e410e75268a
Niv Shah <nivshah@gmail.com>
2017-05-16 15:01:27 -0400

    Working around the new strong_parameters gem

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,7 +15,7 @@ class AdminController < ApplicationController
   end
 
   def create_list
-    @list = List.new(params[:list])
+    @list = List.new(list_params)
   end
 
   def new_link
@@ -23,18 +23,13 @@ class AdminController < ApplicationController
   end
 
   def create_link
-    @link = Link.new(params[:link])
+    @link = Link.create(link_params)
   end
 
   def process_link
     # ajax endpoint that uses gem to process a potential link
     url = params[:url]
-    @title = false # in case a.get fails
-    a = Mechanize.new
-    a.get(url) do |page|
-      @title = page.title
-    end
-
+    @title = get_title(url)
 
     respond_to do |format|
       format.json do
@@ -44,4 +39,23 @@ class AdminController < ApplicationController
       end
     end
   end
+
+  def get_title(url)
+    @title = false
+    a = Mechanize.new
+    a.get(url) do |page|
+      @title = page.title
+    end
+
+    return @title
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:publish_date)
+    end
+
+    def link_params
+      params.require(:link).permit(:url, :title, :category_id)
+    end
 end
