bd2266f397eb510a981c104f451171bfd5211999
Niv Shah <nivshah@gmail.com>
2017-05-17 12:12:45 -0400

    Allow users to change their passwords

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   post 'admin/create_list'
   post 'admin/create_link'
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
