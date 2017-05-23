24eadf621f0fcbf0110b5345a041b321c50a29fe
flanban <johnsfuller@gmail.com>
2017-05-23 18:19:41 -0400

    styling dropdown menu, wip

---
+++ app/assets/javascripts/user-menu.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.icon-user-actions').hover(
+    function () {
+      // show
+      $('.user-nav-items').addClass('show-user-nav');
+    },
+    function () {
+      $('.user-nav-items').removeClass('show-user-nav');
+    }
+  );
+});
