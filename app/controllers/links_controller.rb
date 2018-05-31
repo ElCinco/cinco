6eb582bf938f67588003c142849c9a71bd7fe1d0
gearloan <johnsfuller@gmail.com>
2018-05-31 19:49:00 -0400

    removed voting

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,12 +6,4 @@ class LinksController < ApplicationController
     @link = Link.friendly.find(params[:id])
   end
 
-  def vote_up
-    begin
-      current_or_guest_user.vote_for(@link = Link.find(params[:id]))
-      redirect_back(fallback_location: root_path)
-    rescue ActiveRecord::RecordInvalid
-      render :nothing => true, :status => 404
-    end
-  end
 end
